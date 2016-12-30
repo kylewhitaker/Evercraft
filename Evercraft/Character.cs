d523cb000ee9e6a45e3473e979db7b8d35e88798
Kyle Whitaker <kylewhitaker51@gmail.com>
2016-12-30 16:57:51 -0500

    Add Character files, first unit test, RhinoMocks

---
+++ Evercraft/Character.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Evercraft
+{
+	public class Character
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ EvercraftTests/CharacterTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Evercraft;
+using System;
+
+namespace EvercraftTests
+{
+	[TestFixture()]
+	public class CharacterTests
+	{
+		[Test()]
+		public void Name()
+		{
+			Character c = new Character();
+			c.Name = "Kyle";
+			Assert.AreEqual("Kyle", c.Name);
+		}
+	}
+}
