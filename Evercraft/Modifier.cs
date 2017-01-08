df5207e24bf4e6f695dbd25765ba75edd00ba3e7
Kyle Whitaker <kylewhitaker51@gmail.com>
2017-01-07 20:47:41 -0500

    Add Modifier, test add strength to attack roll

---
+++ Evercraft/Modifier.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Modifier
+{
+	public int AddStrengthToAttackRoll(int roll, int strength)
+	{
+		return roll + strength;
+	}
+}
---
+++ EvercraftTests/ModifierTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+public class ModifierTests
+{
+	[Test]
+	public void AddStrengthToAttackRoll()
+	{
+		Modifier m = new Modifier();
+		int roll = 15, strength = 2;
+		Assert.AreEqual(roll + strength, m.AddStrengthToAttackRoll(roll, strength));
+
+	}
+}
